00b1fbc64b83255611cb5964ccf8b2f29d6aef8b
AMORAND <adrien.morand@ynov.com>
2023-04-27 16:12:22 +0200

    add fake Footer

---
+++ react-starter/src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <div className="footer__links">
+                    <h3>À propos</h3>
+                    <ul>
+                        <li><a href="/about">Qui sommes-nous ?</a></li>
+                        <li><a href="/careers">Carrières</a></li>
+                        <li><a href="/press">Presse</a></li>
+                    </ul>
+                </div>
+                <div className="footer__links">
+                    <h3>Aide</h3>
+                    <ul>
+                        <li><a href="/help-center">Centre d'aide</a></li>
+                        <li><a href="/contact">Contactez-nous</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                    </ul>
+                </div>
+                <div className="footer__links">
+                    <h3>Politiques</h3>
+                    <ul>
+                        <li><a href="/terms">Conditions d'utilisation</a></li>
+                        <li><a href="/privacy">Politique de confidentialité</a></li>
+                        <li><a href="/cookies">Politique en matière de cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer__copyright">
+                &copy; {new Date().getFullYear()} Airbnb Clone. Tous droits réservés.
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
