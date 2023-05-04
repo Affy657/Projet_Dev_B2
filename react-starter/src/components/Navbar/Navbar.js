6095683641ebc4ca7f52a2dc7f967378711cf5ba
AMORAND <adrien.morand@ynov.com>
2023-05-04 14:58:57 +0200

    fix navbar

--- react-starter/src/components/Navbar/Navbar.js
+++ react-starter/src/components/Navbar/Navbar.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import SearchBar from "./SearchBar/SearchBar";
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="logo">React Airbnb</div>
       <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search for a place"
-          className="search-input"
-        />
+        <SearchBar />
       </div>
       <div className="profile-icon">Profile</div>
     </div>
