6568f6a0e7d4524c9ccbbcc252ee264080f79340
AMORAND <adrien.morand@ynov.com>
2023-05-05 16:10:47 +0200

    add easter eggs

--- react-starter/src/components/Footer/Footer.js
+++ react-starter/src/components/Footer/Footer.js
@@ -2,31 +2,33 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
+    const youtubeLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
     return (
         <footer className="footer">
             <div className="footer__container">
                 <div className="footer__links">
                     <h3>À propos</h3>
                     <ul>
-                        <li><a href="/about">Qui sommes-nous ?</a></li>
-                        <li><a href="/careers">Carrières</a></li>
-                        <li><a href="/press">Presse</a></li>
+                        <li><a href={youtubeLink}>Qui sommes-nous ?</a></li>
+                        <li><a href={youtubeLink}>Carrières</a></li>
+                        <li><a href={youtubeLink}>Presse</a></li>
                     </ul>
                 </div>
                 <div className="footer__links">
                     <h3>Aide</h3>
                     <ul>
-                        <li><a href="/help-center">Centre d'aide</a></li>
-                        <li><a href="/contact">Contactez-nous</a></li>
-                        <li><a href="/faq">FAQ</a></li>
+                        <li><a href={youtubeLink}>Centre d'aide</a></li>
+                        <li><a href={youtubeLink}>Contactez-nous</a></li>
+                        <li><a href={youtubeLink}>FAQ</a></li>
                     </ul>
                 </div>
                 <div className="footer__links">
                     <h3>Politiques</h3>
                     <ul>
-                        <li><a href="/terms">Conditions d'utilisation</a></li>
-                        <li><a href="/privacy">Politique de confidentialité</a></li>
-                        <li><a href="/cookies">Politique en matière de cookies</a></li>
+                        <li><a href={youtubeLink}>Conditions d'utilisation</a></li>
+                        <li><a href={youtubeLink}>Politique de confidentialité</a></li>
+                        <li><a href={youtubeLink}>Politique en matière de cookies</a></li>
                     </ul>
                 </div>
             </div>
